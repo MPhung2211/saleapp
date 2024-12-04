7f9ddc76627bbe61eaf8bd3d79067a4aab379c7e
MP <butdeptrai2k@gmail.com>
2024-12-04 16:34:59 +0700

    update

---
+++ app/index.py
@@ -0,0 +1,101 @@
+import math
+from flask import render_template, request, redirect, jsonify, session
+import dao, ultis
+from saleapp.app import app, login
+from flask_login import login_user, logout_user
+
+@app.route("/")
+def index():
+    cates = dao.load_categories()
+
+    cate_id = request.args.get('category_id')
+    kw = request.args.get('kw')
+    page = request.args.get('page', 1)
+    prods = dao.load_products(cate_id=cate_id, kw=kw, page=int(page))
+
+    total = dao.count_products()
+    page_size = app.config['PAGE_SIZE']
+
+    return render_template("index.html", categories=cates, products=prods, pages=math.ceil(total/page_size))
+
+
+@app.route('/login', methods=['get', 'post'])
+def login_process():
+    if request.method.__eq__("POST"):
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = dao.auth_user(username=username, password=password)
+        if user:
+            login_user(user)
+            return redirect('/')
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout_process():
+    logout_user()
+    return redirect('/login')
+
+
+@app.route('/register', methods=['get', 'post'])
+def register_process():
+    err_msg = ''
+    if request.method.__eq__("POST"):
+        password = request.form.get('password')
+        confirm = request.form.get('confirm')
+        if password.__eq__(confirm):
+            data = request.form.copy()
+            del data['confirm']
+            dao.add_user(avatar=request.files.get('avatar'), **data)
+            return redirect('/login')
+        else:
+            err_msg = 'Mật khẩu không khớp!'
+
+    return render_template('register.html', err_msg=err_msg)
+
+@app.route('/api/carts', methods=['post'])
+def add_to_cart():
+    """
+    {
+       "1": {
+            "id": "1",
+            "name": "..",
+            "price": 123,
+            "quantity": 2
+       },  "2": {
+            "id": "2",
+            "name": "..",
+            "price": 123,
+            "quantity": 1
+    }
+    """
+    cart = session.get('cart')
+    if not cart:
+        cart = {}
+
+    id = str(request.json.get('id'))
+    name = request.json.get('name')
+    price = request.json.get('price')
+
+    if id in cart:
+        cart[id]["quantity"] +=1
+    else:
+        cart[id] = {
+            "id": id,
+            "name": name,
+            "price": price,
+            "quantity": 1
+        }
+
+    session['cart'] = cart
+    print(cart)
+    return jsonify(ultis.stats_cart(cart))
+
+@login.user_loader
+def get_user_by_id(user_id):
+    return dao.get_user_by_id(user_id)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
